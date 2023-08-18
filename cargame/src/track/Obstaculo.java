fe9ab87bde49798e435dea21396ecbf9eb0ca519
Armando <armcastro@estudiantec.cr>
2023-08-18 13:59:04 -0600

    Cambios a obstaculo

--- cargame/src/track/Obstaculo.java
+++ cargame/src/track/Obstaculo.java
@@ -1,5 +1,20 @@
 package track;
 
 public class Obstaculo {
+	private String tipoBomba;
+    private int kilotones;
+    private int daño;
+    private int tiempoRestante;
 
+    public Obstaculo(String tipoBomba, int kilotones, int daño, int tiempoRestante) {
+        this.tipoBomba = tipoBomba;
+        this.kilotones = kilotones;
+        this.daño = daño;
+        this.tiempoRestante = tiempoRestante;
+    }
+
+    public void detonar() {
+        this.tiempoRestante = 0;
+        System.out.println("La bomba ha explotado! Inflinge "+this.daño+"de daño.");
+    }
 }
